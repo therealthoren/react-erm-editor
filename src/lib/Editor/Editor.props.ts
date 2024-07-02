2451dbe3b63677c0bbc58b9b9b13f42eb2e48d9d
Thoren Lederer <lederer@adrodev.de>
2024-07-02 08:45:34 +0200

    added files

--- src/lib/Editor/Editor.props.ts
+++ src/lib/Editor/Editor.props.ts
@@ -2,8 +2,17 @@ import { ReactNode } from 'react'
 
 interface IEditorProps extends Partial<Omit<HTMLButtonElement, 'children'>> {
   children?: ReactNode
-  nodes: []
-  edges: []
+  nodes: [],
+  edges: [],
+  onNodesChanged?: (nodes: any) => void
+  onEdgesChanged?: (edges: any) => void
+  onNodeSelected?: (node: any) => void
+  onEdgeSelected?: (edge: any) => void
+  onNodeDeselected?: (node: any) => void
+  onEdgeDeselected?: (edge: any) => void
+  onNodeContextMenu?: (event: any, node: any) => void
+  onAddNode?: (node: any) => void
+  onAddEdge?: (edge: any) => void
 }
 
 export default IEditorProps
